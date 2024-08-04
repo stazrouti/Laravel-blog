be095ebb76ea13f88537a2e4076aff88dcb4e84e
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-08-04 20:13:14 +0100

    update table name

--- database/migrations/2023_10_16_193602_create__contact_table.php
+++ database/migrations/2023_10_16_193602_create__contact_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('contact', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
