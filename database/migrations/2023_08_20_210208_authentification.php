8f12e40fc26d6d49a874ad27050f4dd55ae217b6
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-08-04 20:13:55 +0100

    hide code to create user table again to fixe migration error

--- database/migrations/2023_08_20_210208_authentification.php
+++ database/migrations/2023_08_20_210208_authentification.php
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+/*     public function up(): void
     {
         Schema::create('Users', function (Blueprint $table) {
             $table->id();
@@ -23,8 +23,8 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    /* public function down(): void
     {
         Schema::dropIfExists('Users');
-    }
+    } */
 };
