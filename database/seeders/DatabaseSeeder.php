4354f3b08bb99bb29af87bed361e1d4d767901a8
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-08-04 20:14:12 +0100

    import all tables to seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,12 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\categoriestable;
+use Database\Seeders\poststable;
+use Database\Seeders\Userseader;
+use Database\Seeders\visittable;
+use Database\Seeders\post_likes_table;
+use Database\Seeders\AdminSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +24,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(categoriestable::class);
+        $this->call(poststable::class);
+        $this->call(Userseader::class);
+        $this->call(visittable::class);
+        $this->call(post_likes_table::class);
+        $this->call(AdminSeeder::class);
+
     }
 }
